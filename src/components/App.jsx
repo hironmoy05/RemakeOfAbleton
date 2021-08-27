5a2f335a1753a1711873cfdcfa01b02c08b02e94
gaurav <gaurav@gmail.com>
2021-08-27 19:08:14 +0530

    add sticky header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,34 @@
-import PrimaryHeader from "./PrimaryHeader/PrimaryHeader";
-import SecondaryHeader from "./SecondaryHeader/SecondaryHeader";
-import Hero from "./Hero/Hero";
-import AboutMusic from "./About/AboutMusic/AboutMusic";
-import MakingMusic from "./About/MakingMusic/MakingMusic";
-import AboutUs from "./About/AboutUs/AboutUs";
-import Believe from "./About/Believe/Believe";
-import OurPassionate from "./About/OutPassionate/OurPassionate";
-import Talent from "./About/Talent/Talent";
-import PrimaryFooter from "./PrimaryFooter/PrimaryFooter";
+import PrimaryHeader from './PrimaryHeader/PrimaryHeader';
+import SecondaryHeader from './SecondaryHeader/SecondaryHeader';
+import Hero from './Hero/Hero';
+import AboutMusic from './About/AboutMusic/AboutMusic';
+import MakingMusic from './About/MakingMusic/MakingMusic';
+import AboutUs from './About/AboutUs/AboutUs';
+import Believe from './About/Believe/Believe';
+import OurPassionate from './About/OutPassionate/OurPassionate';
+import Talent from './About/Talent/Talent';
+import PrimaryFooter from './PrimaryFooter/PrimaryFooter';
+import Sticky from 'react-sticky-el';
 
 function App() {
-  return (
-    <>
-      <PrimaryHeader />
-      <SecondaryHeader />
-      <Hero />
-      <main>
-        <AboutMusic />
-        <MakingMusic />
-        <AboutUs />
-        <Believe />
-        <OurPassionate />
-        <Talent />
-      </main>
-      <PrimaryFooter />
-    </>
-  );
+	return (
+		<>
+			<PrimaryHeader />
+			<Sticky topOffset={120} bottomOffset={90}>
+				<SecondaryHeader />
+			</Sticky>
+			<Hero />
+			<main>
+				<AboutMusic />
+				<MakingMusic />
+				<AboutUs />
+				<Believe />
+				<OurPassionate />
+				<Talent />
+			</main>
+			<PrimaryFooter />
+		</>
+	);
 }
 
 export default App;
